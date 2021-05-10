aa08122a7db591922bb96e0456e545c738411513
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-10 21:51:25 +0200

    Prepared SECRET_KEY and changed timezone

--- vapeking/vapeking/settings.py
+++ vapeking/vapeking/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
-print(SECRET_KEY)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 

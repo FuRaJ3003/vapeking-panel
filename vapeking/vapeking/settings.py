d8626d75847079a2e1481250a3b247fa424a485d
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-11 20:47:38 +0200

    initialized all models apps for v1.0

--- vapeking/vapeking/settings.py
+++ vapeking/vapeking/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # own
     'vapeking.user',
+    # thid-party
     'graphene_django',
 ]
 

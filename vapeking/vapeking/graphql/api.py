57f05a57ab0f7d904dbe21c81843ba5e92bf3c34
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-13 10:33:27 +0200

    Added store to API queries

--- vapeking/vapeking/graphql/api.py
+++ vapeking/vapeking/graphql/api.py
@@ -1,8 +1,9 @@
 import graphene
 
 from .user.schema import UserQueries
+from .store.schema import StoreQueries
 
-class Query(UserQueries):
+class Query(UserQueries, StoreQueries):
     pass
 
 schema = graphene.Schema(query=Query)

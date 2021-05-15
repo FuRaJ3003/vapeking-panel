11ee1e8c3b6e0218751e30501cd65559d105d140
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-15 16:54:05 +0200

    Changed foregin key properties in product model

--- vapeking/vapeking/product/models.py
+++ vapeking/vapeking/product/models.py
@@ -3,7 +3,10 @@ from vapeking.type.models import Type
 
 class Product(models.Model):
     p_name = models.CharField(max_length=100, null=False, blank=False)
-    p_type = models.ManyToManyField(Type, related_name='type', default="Inne")
+    p_type = models.ForeignKey(Type, related_name='type', default="Inne", on_delete=models.CASCADE)
     p_price = models.FloatField(null=False, blank=False)
+
+    def __str__(self):
+        return f'Product: {self.p_name}'
 
 

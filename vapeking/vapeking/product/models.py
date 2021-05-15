842e143dc483ab0134aee23feaa69a3d84aa0b37
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-15 17:06:07 +0200

    Restored database, changed product type field

--- vapeking/vapeking/product/models.py
+++ vapeking/vapeking/product/models.py
@@ -3,10 +3,10 @@ from vapeking.type.models import Type
 
 class Product(models.Model):
     p_name = models.CharField(max_length=100, null=False, blank=False)
-    p_type = models.ForeignKey(Type, related_name='type', default="Inne", on_delete=models.CASCADE)
-    p_price = models.FloatField(null=False, blank=False)
+    p_type = models.ForeignKey(Type, related_name='type', on_delete=models.CASCADE)
+    p_price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False, default=0.00)
 
     def __str__(self):
-        return f'Product: {self.p_name}'
+        return f'Product: {self.p_name}PLN'
 
 

e9075bfdb6a214d6e670adbc06af24e9a96e9fd2
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-15 16:54:54 +0200

    Maked migrations and added proper type name disp

--- vapeking/vapeking/type/models.py
+++ vapeking/vapeking/type/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class Type(models.Model):
     t_name = models.TextField(max_length=30, blank=False, null=False)
     
+    def __str__(self):
+        return f'Type: {self.t_name}'

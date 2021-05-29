4be12ed710f19b0e8804f882dfc497f2d80dba99
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-29 14:23:33 +0200

    Added multiple select function and object instance checks'

--- vapeking/vapeking/graphql/product/forms.py
+++ vapeking/vapeking/graphql/product/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from vapeking.product.models import Product
 
 class ProductForm(forms.ModelForm):
-    p_type = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple(), queryset=Type.objects.all())
+    type = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple(), queryset=Type.objects.all())
 
      class Meta:
         model = Product
--- vapeking/vapeking/graphql/store/forms.py
+++ vapeking/vapeking/graphql/store/forms.py
@@ -5,3 +5,27 @@ class StoreForm(forms.ModelForm):
     class Meta:
         model = Store
         fields = ("name", "products", "city")
+
+        widgets = {
+            'products': CheckboxSelectMultiple()
+        }
+
+        products = ModelMultipleChoiceField(
+            queryset=Product.objects.all(),
+            required=False
+            )
+
+    def __init__(self, *args, **kwargs):
+        if 'instance' in kwargs:
+            initial = kwargs.setdefault('initial', {})
+            initial['products'] = [s.pk for s in kwargs['instance'].products.all()]
+
+        ModelForm.__init__(self, *args, **kwargs)
+
+    def save(self, commit=True):
+        store = ModelForm.save(self, False)
+
+        def save_m2m():
+            new_products = self.cleaned_data['products']
+            old_products = store.products.all()
+        

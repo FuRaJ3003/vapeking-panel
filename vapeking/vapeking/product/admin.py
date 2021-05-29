f4c3e68506d9a98e49ade4f267dc1d24950bd0a9
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-05-29 14:24:54 +0200

    Fixed old variable names (deleted model ref prefixes)

--- vapeking/vapeking/product/admin.py
+++ vapeking/vapeking/product/admin.py
@@ -9,21 +9,21 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('get_name', 'get_price', 'get_type')
 
     def get_name(self, obj):
-        return obj.p_name
+        return obj.name
 
     def get_type(self, obj):
-        return obj.p_type
+        return obj.type
 
     def get_price(self, obj):
-        return f"{obj.p_price} PLN"
+        return f"{obj.price} PLN"
     
-    get_name.admin_order_field = 'p_name'
+    get_name.admin_order_field = 'name'
     get_name.short_description = "Nazwa"
 
-    get_type.admin_order_field = 't_name'
+    get_type.admin_order_field = 'type'
     get_type.short_description = 'Typ Produktu'
 
-    get_price.admin_order_field = 'p_price'
+    get_price.admin_order_field = 'price'
     get_price.short_description = "Cena"
 
     # def get_price(self, obj):

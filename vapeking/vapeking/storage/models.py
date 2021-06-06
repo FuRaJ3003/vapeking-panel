e00d249884f839754ed261f590f1de6015bed06b
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-06 17:13:48 +0200

    Added StorageStock model

--- vapeking/vapeking/storage/models.py
+++ vapeking/vapeking/storage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from ..store.models import Store
+from ..product.models import Product
 
-# Create your models here.
+class Storage(models.Model):
+    name = models.CharField(max_length=20, blank=True, null=True) 
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.store})"
+
+class StorageStock(models.Model):
+    storage = models.ForeignKey(Storage, on_delete=models.CASCADE, blank=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=False)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.product.name} ({self.storage.name})"
+
+
+

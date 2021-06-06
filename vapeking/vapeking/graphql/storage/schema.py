3bf6e06fdbfd6ff3b826ebd812ff50b0f3425345
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-06 17:20:18 +0200

    Added storage_id into storage queries

--- vapeking/vapeking/graphql/storage/schema.py
+++ vapeking/vapeking/graphql/storage/schema.py
@@ -2,4 +2,11 @@ import graphene
 
 from .types import StorageType, StorageStockType
 from ...storage.models import Storage, StorageStock
-from  .mutations
+from  .mutations import StorageCreate, StorageStockCreate
+
+class StorageQueries(graphene.ObjectType):
+    storage = graphene.Field(
+        StorageType,
+        id=graphene.Argument(graphene.ID, description="ID of storage.")
+    )
+    storages = graphene.List(StorageType)

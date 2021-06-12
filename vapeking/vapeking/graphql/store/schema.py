b4cb00a9035fa59ebc333878b69bb9c96b965f5d
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-12 07:30:25 +0200

    Secured store resolvers

--- vapeking/vapeking/graphql/store/schema.py
+++ vapeking/vapeking/graphql/store/schema.py
@@ -1,15 +1,17 @@
 import graphene
 from .types import StoreType
 from vapeking.store.models import Store
-
+from .core.utils import staff_member_required, manager_member_required
 
 class StoreQueries(graphene.ObjectType):
     store = graphene.Field(
         StoreType, id=graphene.Argument(graphene.ID, description="ID of Store.")
     )
-
+    
+    @staff_member_required
     def resolve_store(self, _info, id):
         return Store.objects.filter(id=id).first()
 
+    @manager_member_required
     def resolve_stores(self, _info):
         return Store.objects.all()

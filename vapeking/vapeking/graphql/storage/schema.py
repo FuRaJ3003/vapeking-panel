2ef60db881ee01b11e308c8b76af19c0693b87f0
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-12 07:36:46 +0200

    Secured storage resolvers

--- vapeking/vapeking/graphql/storage/schema.py
+++ vapeking/vapeking/graphql/storage/schema.py
@@ -3,6 +3,7 @@ import graphene
 from .types import StorageType, StorageStockType
 from ...storage.models import Storage, StorageStock
 from  .mutations import StorageCreate, StorageStockCreate
+from .core.utils import staff_member_required
 
 class StorageQueries(graphene.ObjectType):
     storage = graphene.Field(
@@ -15,20 +16,24 @@ class StorageQueries(graphene.ObjectType):
         id=graphene.Argument(graphene.ID, description="ID of storage stock."),
     )
     storage_stocks = graphene.List(StorageStockType)
-
+    
+    @staff_member_required
     def resolve_storage(self, _info, id):
         return Storage.objects.filter(id=id).first()
 
+    @staff_member_required
     def resolve_storages(self, _info):
         return Storage.objects.all()
 
-    # def resolve_storage_by_shop(self, info, shop) TODO
-
+    @staff_member_required
     def resolve_storage_stock(self, _info, id):
         return StorageStock.objects.filter(id=id).first()
 
+    @staff_member_required
     def resolve_storage_stocks(self, _info):
         return StorageStock.objects.all()
+
+    # def resolve_storage_by_shop(self, info, shop) TODO
 
 class StorageMutations(graphene.ObjectType):
     storage_create = StorageCreate.Field()

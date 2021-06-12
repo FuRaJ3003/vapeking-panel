cd709afdfc7304a0c1183f542fc7fb3ac1cd2e41
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-12 07:08:54 +0200

    Created user permission error

--- vapeking/vapeking/graphql/core/utils.py
+++ vapeking/vapeking/graphql/core/utils.py
@@ -11,23 +11,6 @@ class AuthenticateMutations(graphene.ObjectType):
 class UserPermissionError(PermissionError):
     def __init__(self, msg='Nie masz uprawnień aby wykonać tę akcję', *args, **kwargs):
         super().__init__(msg, *args, **kwargs)
+ 
 
 
-def check_permission(name, *args):
-    for arg in args:
-        if isinstance(arg, GraphQLResolveInfo):
-            if not getattr(arg.context.user, name):
-                raise UserPermissionError
-
-def superuser_required(func):
-    def check_su(*args, **kwargs):
-        check_permission('is_superuser', *args)
-        return func(*args, **kwargs)
-    return check_su
-
-
-def staff_member_required(func):
-    def check_staff(*args, **kwargs):
-        check_permission('is_staff', *args)
-        return func(*args, **kwargs)
-    return check_staff 

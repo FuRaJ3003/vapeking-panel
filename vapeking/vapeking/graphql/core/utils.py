20890a85736355974d26371bfcffbfed982ef3e7
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-12 07:14:41 +0200

    Added check active, check admin decorator

--- vapeking/vapeking/graphql/core/utils.py
+++ vapeking/vapeking/graphql/core/utils.py
@@ -18,15 +18,37 @@ def check_permission(name, *args):
             if not getattr(arg.context.user, name):
                 raise UserPermissionError
 
-def superuser_required(func):
+
+def superuser_member_required(func):
     def check_su(*args, **kwargs):
         check_permission('is_superuser', *args)
         return func(*args, **kwargs)
     return check_su
 
 
+def admin_member_required(func):
+    def check_admin(*args, **kwargs):
+        check_permission('is_admin', *args)
+        return func(*args, **kwargs)
+    return check_admin
+
+
+def manager_member_required(func):
+    def check_manager(*args, **kwargs):
+        check_permission('is_manager', *args)
+        return func(*args, **kwargs)
+    return check_manager
+
+
 def staff_member_required(func):
     def check_staff(*args, **kwargs):
         check_permission('is_staff', *args)
         return func(*args, **kwargs)
-    return check_staff 
+    return check_staff
+
+
+def active_member_required(func):
+    def check_active(*args, **kwargs):
+        check_permission('is_active', *args)
+        return func(*args, **kwargs)
+    return check_active

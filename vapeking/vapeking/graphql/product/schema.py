c3de160bf65c0609f8e368db5cc24bd3f1c28a0a
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-12 07:36:34 +0200

    Secured product resolvers

--- vapeking/vapeking/graphql/product/schema.py
+++ vapeking/vapeking/graphql/product/schema.py
@@ -1,14 +1,17 @@
 import graphene
 from .types import ProductType
 from vapeking.product.models import Product
+from .core.utils import manager_member_required
 
 class ProductQueries(graphene.ObjectType):
     product = graphene.Field(
         ProductType, id=graphene.Argument(graphene.ID, description="ID of Product.")
     )
 
+    @manager_member_required
     def resolve_product(self, _info, id):
         return Product.objects.filter(id=id).first()
 
+    @manager_member_required
     def resolve_products(self, _info):
         return Product.objects.all()

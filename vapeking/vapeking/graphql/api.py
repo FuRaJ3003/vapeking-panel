f321426c693a4e58df5aa7a27e8fd8134c47ab5d
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-12 07:52:23 +0200

    Linked in authenticate mutations

--- vapeking/vapeking/graphql/api.py
+++ vapeking/vapeking/graphql/api.py
@@ -4,11 +4,12 @@ from .user.schema import UserQueries
 from .store.schema import StoreQueries
 from .product.schema import ProductQueries
 from .storage.schema import StorageQueries, StorageMutations
+from .user.authenticate import AuthenticateMutations
 
 class Query(UserQueries, StoreQueries, StorageQueries):
     pass
 
-class Mutations(StorageMutations):
+class Mutations(StorageMutations, AuthenticateMutations):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutations)

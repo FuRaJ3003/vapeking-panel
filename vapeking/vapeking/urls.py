0ea413c3d1b632d196d129b697533aefbf861d38
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-18 16:28:07 +0200

    Added test docs page

--- vapeking/vapeking/urls.py
+++ vapeking/vapeking/urls.py
@@ -1,22 +1,8 @@
-"""vapeking URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from jet.dashboard.dashboard_modules import google_analytics_views
 from django.contrib import admin
 from django.urls import path, include
 from graphene_django.views import GraphQLView
+from .views import graphql_docs
 
 
 urlpatterns = [
@@ -24,4 +10,5 @@ urlpatterns = [
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('admin/', admin.site.urls),
     path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('docs/', graphql_docs, name='graphql_docs'),
 ]

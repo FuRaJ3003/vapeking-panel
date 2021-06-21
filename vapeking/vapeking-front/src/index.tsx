eb51bff32e68285739dc3b1f5f34f3d2f1c6b9e7
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-21 22:51:29 +0200

    Added routes render into index page

--- vapeking/vapeking-front/src/index.tsx
+++ vapeking/vapeking-front/src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+import registerServiceWorker from './registerServiceWorker';
+import { Routes } from "./Routes";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  credentials: "include"
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();

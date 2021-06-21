7d54fa83ffafda7ae26adbab6f6e3021ed9f6651
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-06-21 23:00:09 +0200

    Added Cross-Origin policy into dev server

--- vapeking/vapeking/settings.py
+++ vapeking/vapeking/settings.py
@@ -78,7 +78,8 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://localhost:8000"
     # frontend React App
 ]
 

16fb7b8fb93be2c5a0b90737010f23da3a043008
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-07-06 13:09:15 +0200

    Added #@#$ slash into uri... >:C

--- vapeking/vapeking-front/src/index.tsx
+++ vapeking/vapeking-front/src/index.tsx
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { Routes } from "./Routes";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  uri: "http://localhost:8000/graphql/",
   credentials: "include"
 });
 

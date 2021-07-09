2a7903f012448f9c903244285007abe934f69055
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-07-09 11:33:30 +0200

    Added test Me View page

--- vapeking/vapeking-front/src/components/user/MeView.tsx
+++ vapeking/vapeking-front/src/components/user/MeView.tsx
@@ -1,7 +1,34 @@
 import * as React from 'react';
+import { Query } from 'react-apollo';
+import { gql } from "apollo-boost";
+
+const meQuery = gql`
+    query MeQuery {
+        userEmail {
+            
+        }
+    }
+`
 
 export class MeView extends React.PureComponent {
     render() {
-        return <div>Me view</div>;
-        }
+        return <Query<MeQuery> query={meQuery}>
+            {({data, loading}) => {
+                if (loading) {
+                    return null;
+                }
+
+                if (!data) {
+                    return <div>Dane nie są zidentyfikowane</div>
+                }
+
+                if (!data.me) {
+                    return <div>Nie znaleziono użytkownika</div>
+                }
+
+                return <div>{data.me.email}</div>
+            }}
+            <div>{email}</div>
+            )</Query>;
+    }
 }

233552657c8abaf76b81857259e5895cd3e2c83d
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-07-13 09:47:18 +0200

    Replaced meview with test website

--- vapeking/vapeking-front/src/components/user/MeView.tsx
+++ vapeking/vapeking-front/src/components/user/MeView.tsx
@@ -1,34 +1,16 @@
 import * as React from 'react';
-import { Query } from 'react-apollo';
-import { gql } from "apollo-boost";
+import { RouteComponentProps } from 'react-router-dom';
 
-const meQuery = gql`
-    query MeQuery {
-        userEmail {
-            
-        }
-    }
-`
 
-export class MeView extends React.PureComponent {
+export class MeView extends React.PureComponent<RouteComponentProps<{}>> {
+
     render() {
-        return <Query<MeQuery> query={meQuery}>
-            {({data, loading}) => {
-                if (loading) {
-                    return null;
-                }
-
-                if (!data) {
-                    return <div>Dane nie są zidentyfikowane</div>
-                }
-
-                if (!data.me) {
-                    return <div>Nie znaleziono użytkownika</div>
-                }
-
-                return <div>{data.me.email}</div>
-            }}
-            <div>{email}</div>
-            )</Query>;
+        return <div style={{ display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        fontSize: "120px",
+}}> KOXUWA :D </div>
     }
 }

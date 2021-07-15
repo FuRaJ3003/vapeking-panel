cb68bb1574d1202aeecb07a21dc270cc9fda7eaf
FuRaJ3003 <FurajChirashi3003@gmail.com>
2021-07-15 11:47:54 +0200

    Added dashboard into app routes

--- vapeking/vapeking-front/src/Routes.tsx
+++ vapeking/vapeking-front/src/Routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { LoginView } from './components/user/LoginView';
 import { MeView } from './components/user/MeView';
 import { RegisterView } from './components/user/RegisterView';
+import { DashboardView } from './components/dashboard/DashboardView';
 
 export class Routes extends React.PureComponent {
     render() {
@@ -11,6 +12,7 @@ export class Routes extends React.PureComponent {
           <Switch>
             <Route path="/login" component={LoginView} />
             <Route path="/register" component={RegisterView} />
+            <Route path="/dashboard" component={DashboardView} />
             <Route path="/me" component={MeView} />
           </Switch>
         </BrowserRouter>
